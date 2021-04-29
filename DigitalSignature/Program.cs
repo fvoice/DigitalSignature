eafd72a604723f09188e5060be81feb7bf6891b2
fvoice <fvoice@yandex.ru>
2021-04-29 16:18:55 +0200

    Initial commit

---
+++ DigitalSignature/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Xml;
+using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Xml;
+using System.Xml;
+
+namespace DigitalSignature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Create a new CspParameters object to specify
+                // a key container.
+                CspParameters cspParams = new CspParameters();
+                cspParams.KeyContainerName = "XML_DSIG_RSA_KEY";
+
+                // Create a new RSA signing key and save it in the container.
+                RSACryptoServiceProvider rsaKey = new RSACryptoServiceProvider(cspParams);
+
+                // Create a new XML document.
+                XmlDocument xmlDoc = new XmlDocument();
+
+                // Load an XML file into the XmlDocument object.
+                xmlDoc.PreserveWhitespace = true;
+                xmlDoc.Load("test.xml");
+
+                // Sign the XML document.
+                SignXml(xmlDoc, rsaKey);
+
+                Console.WriteLine("XML file signed.");
+
+                // Save the document.
+                xmlDoc.Save("test.xml");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        // Sign an XML file.
+        // This document cannot be verified unless the verifying
+        // code has the key with which it was signed.
+        public static void SignXml(XmlDocument xmlDoc, RSA rsaKey)
+        {
+            // Check arguments.
+            if (xmlDoc == null)
+                throw new ArgumentException(nameof(xmlDoc));
+            if (rsaKey == null)
+                throw new ArgumentException(nameof(rsaKey));
+
+            // Create a SignedXml object.
+            SignedXml signedXml = new SignedXml(xmlDoc);
+
+            // Add the key to the SignedXml document.
+            signedXml.SigningKey = rsaKey;
+
+            // Create a reference to be signed.
+            Reference reference = new Reference();
+            reference.Uri = "";
+
+            // Add an enveloped transformation to the reference.
+            XmlDsigEnvelopedSignatureTransform env = new XmlDsigEnvelopedSignatureTransform();
+            reference.AddTransform(env);
+
+            // Add the reference to the SignedXml object.
+            signedXml.AddReference(reference);
+
+            // Compute the signature.
+            signedXml.ComputeSignature();
+
+            // Get the XML representation of the signature and save
+            // it to an XmlElement object.
+            XmlElement xmlDigitalSignature = signedXml.GetXml();
+
+            // Append the element to the XML document.
+            xmlDoc.DocumentElement.AppendChild(xmlDoc.ImportNode(xmlDigitalSignature, true));
+        }
+    }
+}
---
+++ DigitalSignatureValidation/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Xml;
+using System.Xml;
+
+namespace DigitalSignatureValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Create a new CspParameters object to specify
+                // a key container.
+                CspParameters cspParams = new CspParameters();
+                cspParams.KeyContainerName = "XML_DSIG_RSA_KEY1";
+
+                // Create a new RSA signing key and save it in the container.
+                RSACryptoServiceProvider rsaKey = new RSACryptoServiceProvider(cspParams);
+
+                // Create a new XML document.
+                XmlDocument xmlDoc = new XmlDocument();
+
+                // Load an XML file into the XmlDocument object.
+                xmlDoc.PreserveWhitespace = true;
+                xmlDoc.Load("test.xml");
+
+                // Verify the signature of the signed XML.
+                Console.WriteLine("Verifying signature...");
+                bool result = VerifyXml(xmlDoc, rsaKey);
+
+                // Display the results of the signature verification to
+                // the console.
+                if (result)
+                {
+                    Console.WriteLine("The XML signature is valid.");
+                }
+                else
+                {
+                    Console.WriteLine("The XML signature is not valid.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        // Verify the signature of an XML file against an asymmetric
+        // algorithm and return the result.
+        public static Boolean VerifyXml(XmlDocument xmlDoc, RSA key)
+        {
+            // Check arguments.
+            if (xmlDoc == null)
+                throw new ArgumentException("xmlDoc");
+            if (key == null)
+                throw new ArgumentException("key");
+
+            // Create a new SignedXml object and pass it
+            // the XML document class.
+            SignedXml signedXml = new SignedXml(xmlDoc);
+
+            // Find the "Signature" node and create a new
+            // XmlNodeList object.
+            XmlNodeList nodeList = xmlDoc.GetElementsByTagName("Signature");
+
+            // Throw an exception if no signature was found.
+            if (nodeList.Count <= 0)
+            {
+                throw new CryptographicException("Verification failed: No Signature was found in the document.");
+            }
+
+            // This example only supports one signature for
+            // the entire XML document.  Throw an exception
+            // if more than one signature was found.
+            if (nodeList.Count >= 2)
+            {
+                throw new CryptographicException("Verification failed: More that one signature was found for the document.");
+            }
+
+            // Load the first <signature> node.
+            signedXml.LoadXml((XmlElement)nodeList[0]);
+
+            // Check the signature and return the result.
+            return signedXml.CheckSignature(key);
+        }
+    }
+}
